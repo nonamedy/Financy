bb89c2451e614d222c671cbdb3813f62e693d7dc
FJ <84997440+nonamedy@users.noreply.github.com>
2025-04-24 08:51:42 -0300

    Class Master
    Criei uma class que  será responsável por criar tudo de maneira simples

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -3,41 +3,11 @@ import {  DataBase } from "../scripts/modules/database/db.mjs";
 import { InputEvents } from "../scripts/modules/HtmlEvents/events.mjs";
 let bud = document.querySelector('.budgets-container');
 
-class CreateBudgets {
-    constructor(parameters) {
-        
-    };
-
-
-    tableBuilder(){
-
-         this.table = new Table('table',bud,'nãotem');
-         return this;
-
-    };
-
-    CompBuilder(){
-
-        this.component = new components('section',bud,'card')
-        return this;
-
-    }
-
-    ExtraBuildr(pai){
-
-        this.extra = new AdicionalInfo('section',bud,'')
-        return this;
-
-    };
-};
-
-
-
 
 
 const table = new Table('table',bud,'nãotem');
 const Tela = new components('section',bud,'card');
-const budgets = ['Gastos Fixos','Investimentos','Metas,','prazeres'];
+
 const comp = new AdicionalInfo('section',document.querySelectorAll('tbody')[0]);
 const banco = new DataBase('Teste')
 
@@ -48,41 +18,21 @@ eventos.EventosParaARenda()
 
 function cria_sesao(BudgetName){
 
-    let inputName = Tela.CreateInput('text','edite o custo','','Edit-Field-input')
-    let inputValue = Tela.CreateInput('number','edite o custo','','Edit-Field-input')
+    let inputName = Tela.CreateInput('text','edite o custo','','Edit-Field-input');
+    let inputValue = Tela.CreateInput('number','edite o custo','','Edit-Field-input');
 
     let table1 = table.CreateTable(3,1,['Nome','Valor'],['Academia','80'],inputName,inputValue);
-    let component  = Tela.CreateComponent(BudgetName,table1)
+    let component  = Tela.CreateComponent(BudgetName,table1);
     const complemento = new AdicionalInfo('section',component,'');
-    complemento.CreateComplement(true)
+    complemento.CreateComplement(true);
 
 }
 
-function Cria_Resumo(x){
-    x = budgets.length
-  
-    let kk = table.CreateTable(x,1,['Budget','Valor Gasto','Deve Gastar','Utilizado','Total'],[budgets])
-  
-    document.querySelectorAll('.card')[1].appendChild(kk)
 
-}
 
-function cria_metas(x){
-
-    x = budgets.length
-  
-    let kk = table.CreateTable(x,1,[' ',' '],[budgets])
-  
-    document.querySelectorAll('.card')[2].appendChild(kk)
-
-        
-}
-
-Cria_Resumo(budgets)
 cria_sesao('Gastos Fixos')
 
 cria_sesao('Investimentos')
-cria_metas()
 
 cria_sesao('Metas')
 cria_sesao('Prazeres')
@@ -100,17 +50,57 @@ function cria_database(){
 
 }
 
-cria_database()
-document.querySelectorAll('table')[0].insertAdjacentElement('afterend',comp.CreateComplement(false));
+cria_database();
 
-const kkk = new CreateBudgets();
-kkk.tableBuilder();
-kkk.CompBuilder();
-kkk.ExtraBuildr();
 
-console.log(kkk);
-const inputName = kkk.component.CreateInput('text','edite o custo','','Edit-Field-input')
-const inputValue = kkk.component.CreateInput('number','edite o custo','','Edit-Field-input')
-const tabela = kkk.table.CreateTable(3,1,['teste','testavel','kkk'],['Academia','80'],inputName,inputValue)
-const compo = kkk.component.CreateComponent('slamanokk',tabela)
-console.log(compo)
+
+
+class teste{
+
+    constructor(){
+
+        this.database = new DataBase('Teste');
+        this.tabela =  new Table('table',bud,'nãotem');
+        this.componentes = new components('section',document.querySelector('.budgets-container'),'card');
+        this.infoadicional = new AdicionalInfo('section',document.querySelector('.budgets-container'));
+
+        this.fixedbudgets = ['Gastos Fixos','Investimentos','Metas','prazeres'];
+    };
+
+    CreateOverview(){
+
+    const budgets = ['Gastos Fixos','Investimentos','Metas','prazeres'];
+  
+    let tabelaresumo = this.tabela.CreateTable(budgets.length,1,['Budget','Valor Gasto','Deve Gastar','Utilizado','Total'],[budgets])
+    let addinfo = this.infoadicional.CreateComplement(true)
+    document.querySelectorAll('.card')[1].appendChild(tabelaresumo)
+    document.querySelectorAll('.card')[1].appendChild(addinfo)
+
+    };
+
+    CreateBudgets(BudgetName){
+
+        let inputName = this.componentes.CreateInput('text','Nome do custo','','Edit-Field-input')
+        let inputValue = Tela.CreateInput('number','R$ 0,00','','Edit-Field-input')
+    
+        let table1 = table.CreateTable(3,1,['Nome','Valor'],['Academia','80'],inputName,inputValue);
+        let component  = Tela.CreateComponent(BudgetName,table1)
+        const complemento = new AdicionalInfo('section',component,'');
+        complemento.CreateComplement(false)
+
+        return component
+    };
+
+    CreateGoals(){
+    
+
+        
+    };
+
+
+}
+
+const sla = new teste()
+sla.CreateOverview()
+
+console.log(sla)
